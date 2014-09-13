e163dfad186426284b8fb96cdf1f88de178c843b
Purencool <purencool@gmail.com>
2014-09-13 16:33:27 +1000

    Update to templates

--- templates/project_details.php
+++ templates/project_details.php
@@ -21,140 +21,83 @@
 
 	print_unescaped($this->inc('tabs'));
 ?>
-
 <div id="app-content">
-	<h1 id="title">
-		<?php p($l->t('Project Details')); ?>
-	</h1>
+	<div id="content-header" >
+	  <h1 id="title"><?php p($l->t('Project Details')); ?></h1>
+	</div>
+	<div id="content-body" >
+	  <table id="details">
+			<tr>
+			  <td><?php p($l->t('Project Title')); ?></td>
+				<td><?php p($_['project_details']['title']); ?></td>
+		  </tr>
 
-	<table id="details">
-		<tr>
-			<td>
-				<?php p($l->t('Project Title')); ?>
-			</td>
+		  <tr>
+			  <td><?php p($l->t('Description')); ?></td>
+			  <td><?php p($_['project_details']['description']); ?></td>
+		  </tr>
 
-			<td>
-				:
-			</td>
+		  <tr>
+		  	<td><?php p($l->t('Deadline')); ?></td>
+			  <td>
+			       <?php
+				 	     p(OC_Collaboration_Time::convertDBTimeToUIDate($_['project_details']['ending_date']));
+				     ?>
+			   </td>
+		  </tr>
 
-			<td>
-				<?php
-					p($_['project_details']['title']);
-				?>
-			</td>
-		</tr>
+		    <tr>
+			    <td><?php p($l->t('Status')); ?></td>
+			    <td id="project_status" >
+				   <?php
+					   if($_['project_details']['completed'] == true) {
+						   p($l->t('Completed on %s', array($l->l('date', OC_Collaboration_Time::convertDBTimeToUIDate($_['project_details']['last_updated'])))));
+							 if(isset($_['msg'])) {
+							  	print_unescaped('<span id="message" > (' . $_['msg'] . ')</span>');
+							 }
+					   } else {
+						     p($l->t('In progress'));
+					   }
+				   ?>
+			    </td>
+		    </tr>
 
-		<tr>
-			<td>
-				<?php p($l->t('Description')); ?>
-			</td>
+		     <tr>
+			    <td><?php p($l->t('Members')); ?></td>
+			    <td></td>
+		     </tr>
+	     </table>
 
-			<td>
-				:
-			</td>
+	     <table id="members_list" >
+         <?php
+			      $member_role = OC_Collaboration_Project::getMembers($_['project_details']['pid']);
 
-			<td>
-				<?php p($_['project_details']['description']); ?>
-			</td>
-		</tr>
+			      if(count($member_role) != 0) { ?>
+				    <tr>
+					    <th><?php p($l->t('Role')); ?></th>
+					    <th><?php p($l->t('Member(s)')); ?></th>
+				    </tr>
+		     <?php } ?>
+        <?php
+			    foreach($member_role as $role => $members) {
+				    print_unescaped('<tr><td>' . $role . '</td><td>');
 
-		<tr>
-			<td>
-				<?php p($l->t('Deadline')); ?>
-			</td>
+				    foreach($members as $index => $member)  {
+					     print_unescaped($member . '<br />');
+				    }
 
-			<td>
-				:
-			</td>
+				    print_unescaped('</td></tr>');
+			    }
+        ?>
+	    </table>
 
-			<td>
-				<?php
-					p(OC_Collaboration_Time::convertDBTimeToUIDate($_['project_details']['ending_date']));
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<?php p($l->t('Status')); ?>
-			</td>
-
-			<td>
-				:
-			</td>
-
-			<td id="project_status" >
-				<?php
-					if($_['project_details']['completed'] == true)
-					{
-						p($l->t('Completed on %s', array($l->l('date', OC_Collaboration_Time::convertDBTimeToUIDate($_['project_details']['last_updated'])))));
-							if(isset($_['msg']))
-							{
-								print_unescaped('<span id="message" > (' . $_['msg'] . ')</span>');
-							}
-					}
-					else
-					{
-						p($l->t('In progress'));
-					}
-				?>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<?php p($l->t('Members')); ?>
-			</td>
-
-			<td>
-				:
-			</td>
-
-			<td>
-			</td>
-		</tr>
-
-	</table>
-
-	<table id="members_list" >
-
-		<?php
-			$member_role = OC_Collaboration_Project::getMembers($_['project_details']['pid']);
-
-			if(count($member_role) != 0)
-			{
-		?>
-				<tr>
-					<th><?php p($l->t('Role')); ?></th>
-					<th><?php p($l->t('Member(s)')); ?></th>
-				</tr>
-		<?php
-			}
-
-			foreach($member_role as $role => $members)
-			{
-				print_unescaped('<tr><td>' . $role . '</td><td>');
-
-				foreach($members as $index => $member)
-				{
-					print_unescaped($member . '<br />');
-				}
-
-				print_unescaped('</td></tr>');
-			}
-
-		?>
-	</table>
-
-	<?php
-		if(OC_Collaboration_Project::isProjectCreatedByMember($_['project_details']['pid'], OC_User::getUser()))
-		{
-	?>
+	    <?php
+		    if(OC_Collaboration_Project::isProjectCreatedByMember($_['project_details']['pid'], OC_User::getUser()))
+		    {
+	    ?>
 			<form action="<?php print_unescaped(OCP\Util::linkToRoute('collaboration_route', array('rel_path'=>'update_project'))); ?>" method="post" >
 				<input type="hidden" name="pid" value="<?php p($_['project_details']['pid']); ?>" />
 				<input type="submit" value="<?php p($l->t('Edit')); ?>" id="edit_project_btn" disabled />
 			</form>
-	<?php
-		}
-	?>
+	    <?php }  ?>
 </div>
